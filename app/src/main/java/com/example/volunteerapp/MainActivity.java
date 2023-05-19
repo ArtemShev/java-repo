21012f5b528e54ed4cfb3ad919432882bcf099ca
DisaniaS <serzh.30303@mail.ru>
2023-05-19 17:55:53 +0300

    signUpPage

--- app/src/main/java/com/example/volunteerapp/MainActivity.java
+++ app/src/main/java/com/example/volunteerapp/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.volunteerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.parse.ParseObject;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button signUpButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signUpButton = findViewById(R.id.signUpButton);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+//                finish();
+            }
+        });
+//        ParseObject firstObject = new  ParseObject("FirstClass");
+//        firstObject.put("message","pasha");
+//        firstObject.put("name","pavel");
+
     }
 }
